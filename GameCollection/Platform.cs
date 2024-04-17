69ebc3cb30553760ea6ebcfa410b09ae2ffdcf5c
Mariana Naselli <nasellimariana@gmail.com>
2024-04-17 17:07:37 +0200

    Created Game

---
+++ GameCollection/Platform.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCollection
+{
+    public class Platform
+    {
+        public enum PlatformType
+        {
+            Console,
+            PC,
+            Mobile,
+ 
+        }
+        public Platform(int id, string name, PlatformType type)
+        {
+            Id = id;
+            Name = name; //Xbox,PlayStation,Pc, etc
+            Type = type; //consola",pc,móvil, et
+        }
+
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public PlatformType Type { get; set; }
+
+
+    }
+}
---
+++ GameCollection/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCollection
+{
+    public class User
+    {
+        public User(int id, string username, List<Game> gameCollection)
+        {
+            Id = id;
+            Username = username;
+            GameCollection = gameCollection;
+        }
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public List<Game> GameCollection { get; set; }
+    }
+}
